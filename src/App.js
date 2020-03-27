083c898a500e5bf914ecdb52ae9ccb450070d39d
josephlaotzu <josephlao.1398@gmail.com>
2020-03-27 00:09:43 -0400

    add info card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Card from "./components/Card";
 import { useFetch } from "./utils/customHooks";
+import WashHands from './img/01.svg';
+import CoverMouth from './img/02.svg';
+import Isolate from './img/03.svg';
+import AvoidContact from './img/04.svg';
+
+
 
 function App() {
   const [country, setCountry] = useState("");
@@ -19,6 +24,27 @@ function App() {
       <main className="main section">
         <Search selectCountry={setCountry} />
         <Card country={parsedCountry} data={data} />
+        <div className="notice">
+          <p>There’s currently no vaccine to prevent coronavirus disease (COVID-19). You can protect yourself and help prevent spreading the virus to others if you:</p>
+        </div>
+
+        {/* todo create info card component */}
+        <div className="card info">
+          <img src={AvoidContact} alt="avoid contact"/>
+          <p>Avoid close contact (1 meter or 3 feet) with people who are unwell</p>
+        </div>
+        <div className="card info">
+          <img src={WashHands} alt="wash hands"/>
+          <p>Wash your hands regularly for 20 seconds, with soap and water or alcohol-based hand rub</p>
+        </div>
+        <div className="card info">
+          <img src={CoverMouth} alt="cover mouth"/>
+          <p>Cover your nose and mouth with a disposable tissue or flexed elbow when you cough or sneeze</p>
+        </div>
+        <div className="card info">
+          <img src={Isolate} alt="cover mouth"/>
+          <p>Stay home and self-isolate from others in the household if you feel unwell</p>
+        </div>
       </main>
     </>
   );
